6edaa6e39230db2a0c3eef4963323066abc5805f
Bondarenko Vadim <vadim_2991@ukr.net>
2016-10-17 23:30:06 +0300

    add TemplatingAwareMailHandlerTrait tests

---
+++ tests/MailHandler/TemplatingAwareMailHandlerTraitTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace VisualCraft\Bundle\MailerBundle\Tests\MailHandler;
+
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use VisualCraft\Bundle\MailerBundle\MailHandler\TemplatingAwareMailHandlerTrait;
+
+class TemplatingAwareMailHandlerTraitTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRenderBody()
+    {
+        $templateName = 'boo.html.twig';
+        $templateParameters = ['bar' => 'foo'];
+        $willReturn = 'foo';
+        $templating = $this->getMockForAbstractClass(EngineInterface::class);
+        $templating
+            ->expects(self::once())
+            ->method('render')
+            ->with($templateName, $templateParameters)
+            ->willReturn($willReturn)
+        ;
+
+        $templatingAwareMailHandler = $this->getMockForTrait(TemplatingAwareMailHandlerTrait::class);
+        $templatingAwareMailHandler->setTemplatingEngine($templating);
+        $closure = function ($template, $parameters) {
+            /** @var $this TemplatingAwareMailHandlerTrait */
+            return $this->renderBody($template, $parameters);
+        };
+        $method = $closure->bindTo($templatingAwareMailHandler, $templatingAwareMailHandler);
+
+        self::assertSame($willReturn, $method($templateName, $templateParameters));
+    }
+
+    public function testRenderSubjectWillBeCalled()
+    {
+        $templateName = 'boo.html.twig';
+        $templateParameters = ['bar' => 'foo'];
+        $templating = $this->getMockForAbstractClass(EngineInterface::class);
+        $templating
+            ->expects(self::once())
+            ->method('render')
+            ->with($templateName, $templateParameters)
+        ;
+
+        $templatingAwareMailHandler = $this->getMockForTrait(TemplatingAwareMailHandlerTrait::class);
+        $templatingAwareMailHandler->setTemplatingEngine($templating);
+        $closure = function ($template, $parameters) {
+            /** @var $this TemplatingAwareMailHandlerTrait */
+            return $this->renderSubject($template, $parameters);
+        };
+        $method = $closure->bindTo($templatingAwareMailHandler, $templatingAwareMailHandler);
+        $method($templateName, $templateParameters);
+    }
+
+    /**
+     * @dataProvider getRenderSubjectData
+     *
+     * @param string $templateEngineReturn
+     * @param string $expectedReturn
+     */
+    public function testRenderSubject($templateEngineReturn, $expectedReturn)
+    {
+        $templating = $this->getMockForAbstractClass(EngineInterface::class);
+        $templating
+            ->method('render')
+            ->willReturn($templateEngineReturn)
+        ;
+
+        $templatingAwareMailHandler = $this->getMockForTrait(TemplatingAwareMailHandlerTrait::class);
+        $templatingAwareMailHandler->setTemplatingEngine($templating);
+        $closure = function ($template, $parameters) {
+            /** @var $this TemplatingAwareMailHandlerTrait */
+            return $this->renderSubject($template, $parameters);
+        };
+        $method = $closure->bindTo($templatingAwareMailHandler, $templatingAwareMailHandler);
+
+        self::assertSame($expectedReturn, $method('boo.html.twig', ['bar' => 'bar']));
+    }
+
+    /**
+     * @return array
+     */
+    public function getRenderSubjectData()
+    {
+        return [
+            ['foo', 'foo'],
+            [' foo ', 'foo'],
+            ["foo\n", 'foo'],
+            [null, ''],
+        ];
+    }
+}
