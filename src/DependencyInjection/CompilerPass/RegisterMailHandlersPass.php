607f971bf24f80a5785e537658eb050e1a52a411
Bondarenko Vadim <vadim_2991@ukr.net>
2016-10-19 23:16:36 +0300

    fix register private and abbstract handlers

--- src/DependencyInjection/CompilerPass/RegisterMailHandlersPass.php
+++ src/DependencyInjection/CompilerPass/RegisterMailHandlersPass.php
@@ -4,6 +4,7 @@ namespace VisualCraft\Bundle\MailerBundle\DependencyInjection\CompilerPass;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
 
 class RegisterMailHandlersPass implements CompilerPassInterface
 {
@@ -16,6 +17,22 @@ class RegisterMailHandlersPass implements CompilerPassInterface
         $handlers = $container->findTaggedServiceIds('visual_craft_mailer.mail_handler');
 
         foreach ($handlers as $id => $attributes) {
+            $definition = $container->getDefinition($id);
+
+            if (!$definition->isPublic()) {
+                throw new InvalidArgumentException(sprintf(
+                    'The service "%s" must be public as it can be lazy-loaded.',
+                    $id
+                ));
+            }
+
+            if ($definition->isAbstract()) {
+                throw new InvalidArgumentException(sprintf(
+                    'The service "%s" must not be abstract as it can be lazy-loaded.',
+                    $id
+                ));
+            }
+
             foreach ($attributes as $attribute) {
                 $registry
                     ->addMethodCall('registerMailHandler', [$attribute['alias'], $id])
