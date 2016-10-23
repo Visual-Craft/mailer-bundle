752251540284ac1ab7af3d50f7fde28d8e8de44b
Bondarenko Vadim <vadim_2991@ukr.net>
2016-10-23 20:29:07 +0300

    change register mailer handler pass  tests

--- tests/DependencyInjection/CompilerPass/RegisterMailHandlersPassTest.php
+++ tests/DependencyInjection/CompilerPass/RegisterMailHandlersPassTest.php
@@ -2,7 +2,10 @@
 
 namespace VisualCraft\Bundle\MailerBundle\Tests\DependencyInjection\CompilerPass;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use VisualCraft\Bundle\MailerBundle\DependencyInjection\CompilerPass\RegisterMailHandlersPass;
+use VisualCraft\Bundle\MailerBundle\MailHandlerInterface;
 
 class RegisterMailHandlersPassTest extends \PHPUnit_Framework_TestCase
 {
@@ -15,29 +18,57 @@ class RegisterMailHandlersPassTest extends \PHPUnit_Framework_TestCase
                 ['alias' => 'my_alias3'],
             ],
         ];
-        $mailHandlerRegistryDefinition = $this->getMock('Symfony\Component\DependencyInjection\Definition');
+        $mailHandlerRegistryDefinition = $this->getMock(Definition::class);
         $container = $this->getMock(
-            'Symfony\Component\DependencyInjection\ContainerBuilder',
+            ContainerBuilder::class,
             ['findTaggedServiceIds', 'getDefinition']
         );
-        $container->expects(self::once())
+        $container
             ->method('getDefinition')
-            ->with('visual_craft_mailer.mail_handler_registry.lazy')
-            ->willReturn($mailHandlerRegistryDefinition)
+            ->willReturnMap([
+                ['visual_craft_mailer.mail_handler_registry.lazy', $mailHandlerRegistryDefinition],
+                ['my_mail_handler_service1', $this->createMailHandlerDefinition()],
+                ['my_mail_handler_service2', $this->createMailHandlerDefinition()],
+            ])
         ;
         $container->expects(self::exactly(1))
             ->method('findTaggedServiceIds')
-            ->will(self::returnValue($services))
+            ->willReturn($services)
         ;
-        $mailHandlerRegistryDefinition->expects(self::exactly(3))
-            ->method('addMethodCall')
-            ->withConsecutive(
-                [self::equalTo('registerMailHandler'), self::equalTo(['my_alias1', 'my_mail_handler_service1'])],
-                [self::equalTo('registerMailHandler'), self::equalTo(['my_alias2', 'my_mail_handler_service2'])],
-                [self::equalTo('registerMailHandler'), self::equalTo(['my_alias3', 'my_mail_handler_service2'])]
-            )
+
+        $mailHandlerRegistryDefinition
+            ->expects(self::once())
+            ->method('replaceArgument')
+            ->with(1, [
+                'my_alias1' => 'my_mail_handler_service1',
+                'my_alias2' => 'my_mail_handler_service2',
+                'my_alias3' => 'my_mail_handler_service2',
+            ])
         ;
         $registerMailHandlersPass = new RegisterMailHandlersPass();
         $registerMailHandlersPass->process($container);
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|Definition
+     */
+    private function createMailHandlerDefinition()
+    {
+        $definition = $this->getMock(Definition::class);
+        $mailHandlerClass = get_class($this->getMockForAbstractClass(MailHandlerInterface::class));
+        $definition
+            ->method('getClass')
+            ->willReturn($mailHandlerClass)
+        ;
+        $definition
+            ->method('isAbstract')
+            ->willReturn(false)
+        ;
+        $definition
+            ->method('isPublic')
+            ->willReturn(true)
+        ;
+
+        return $definition;
     }
 }
